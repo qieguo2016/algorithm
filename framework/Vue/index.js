7578d1a70a8079a0f8d26c67c4930468ee52ef52
qieguo <qieguo.chow@gmail.com>
2016-12-13 00:41:17 +0800

    vue analyse

---
+++ framework/Vue/index.js
@@ -0,0 +1,67 @@
+/**
+ * @authors     : qieguo
+ * @date        : 2016/12/12 0012
+ * @version     : 1.0
+ * @description :
+ */
+
+'use strict';
+
+// observer
+function observe(data) {
+  // 设置开始和递归终止条件
+  if (!data || typeof data !== 'object') {
+    return;
+  }
+  // 不能直接使用for循环，避开闭包陷阱
+  Object.keys(data).forEach(function (key) {
+    defineReactive(data, key, data[key]);
+  })
+}
+
+function defineReactive(data, key, val) {
+  var dep = new Dep();
+  observe(val);   // 递归对象属性到基本类型为止
+  Object.defineProperty(data, key, {
+    enumerable: true,    // 枚举
+    configurable: false, // 不可再配置
+    get: function () {
+      console.log('getter');
+      // 由于需要在闭包内添加watcher，所以通过Dep定义一个全局target属性，暂存watcher, 添加完移除
+      Dep.target && dep.addSub(Dep.target);
+      return val;
+    },
+    set: function (newVal) {
+      if (val === newVal) {
+        return;
+      }
+      console.log('setter');
+      val = newVal;   // setter本身已经做了赋值
+      // 触发通知
+      dep.notify();
+    },
+  })
+}
+
+// depend
+var Dep = function () {
+  this.subs = {};
+};
+
+Dep.prototype.addSub = function (target) {
+  if (!this.subs[target.uid]) {
+    //防止重复添加
+    this.subs[target.uid] = target;
+  }
+};
+
+Dep.prototype.notify = function (newVal) {
+  for (var uid in this.subs) {
+    this.subs[uid].update(newVal);
+  }
+};
+
+// compile
+function Compile() {
+
+}
