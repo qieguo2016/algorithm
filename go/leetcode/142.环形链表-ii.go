b8e60343bc86bbbc1495319f3038ecbafadef532
qieguo <qieguo.chow@gmail.com>
2019-06-23 13:56:50 +0800

    feat-leetcode: add solve for cycle link
    Change-Id: Ic8ca5b7fdf865541871b6ac169def6ac3ddb8d6d

---
+++ go/leetcode/142.环形链表-ii.go
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=golang
+ *
+ * [142] 环形链表 II
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (39.49%)
+ * Likes:    168
+ * Dislikes: 0
+ * Total Accepted:    16.9K
+ * Total Submissions: 42.7K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 说明：不允许修改给定的链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 你是否可以不用额外空间解决此题？
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	slow = head
+	fast = fast
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return fast
+}
+
