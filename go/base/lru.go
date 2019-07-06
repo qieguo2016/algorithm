6e5823c6a545546c9fd3ba5f5a2930e0542ea613
qieguo <qieguo.chow@gmail.com>
2019-07-06 23:02:34 +0800

    feat-base: update lru
    Change-Id: I32e8c6739cad9607fdd5ff4ff78ebd3e9375aae1

--- go/base/lru.go
+++ go/base/lru.go
@@ -1,57 +1,93 @@
 package base
 
-import (
-	"errors"
-)
-
-// LruCache, 基于内存实现、不带过期时间
+// LruCache 基于内存实现、不带过期时间
 // 原理：map结构按照kv存储数据，双向链表保存数据新鲜度
 // 扩展：支持过期时间可以增加一个双向链表按过期时间存储，
-type LruCache struct {
-	store    map[int]int
-	list     *DualLinkList
+
+// LRUChainNode 链表节点
+type LRUChainNode struct {
+	pre   *LRUChainNode
+	next  *LRUChainNode
+	key   int
+	value int
+	ts    int32
+}
+
+// LRUCache 结构
+type LRUCache struct {
 	capacity int
 	length   int
+	store    map[int]*LRUChainNode
+	head     *LRUChainNode
+	tail     *LRUChainNode
 }
 
-// NewLruCache return LRUCache ins
-func NewLruCache(c int) *LruCache {
-	return &LruCache{
-		store:    map[int]int{},
-		list:     &DualLinkList{length: 0, head: nil, tail: nil},
-		capacity: c,
+// NewLRUCache constructor
+func NewLRUCache(capacity int) LRUCache {
+	return LRUCache{
+		capacity: capacity,
 		length:   0,
+		store:    map[int]*LRUChainNode{},
 	}
 }
 
-func (c *LruCache) Put(k int, v int) error {
-	c.store[k] = v
-	c.list.DeleteByValue(k)
-	c.list.Append(k)
-	if c.length >= c.capacity {
-		node := c.list.Pop()
-		delete(c.store, node.value)
-	} else {
-		c.length++
+// Delete 删除key
+func (c *LRUCache) Delete(key int) {
+	node, exist := c.store[key]
+	if !exist {
+		return
 	}
-	return nil
-}
-
-func (c *LruCache) Get(k int) (int, error) {
-	if v, ok := c.store[k]; ok {
-		c.list.DeleteByValue(k)
-		c.list.Insert(k)
-		return v, nil
-	}
-	return -1, errors.New("not found")
-}
-
-func (c *LruCache) Delete(k int) error {
-	if _, ok := c.store[k]; ok {
-		c.list.DeleteByValue(k)
-		delete(c.store, k)
+	delete(c.store, key)
+	if c.length == 1 {
+		c.head = nil
+		c.tail = nil
 		c.length--
-		return nil
+		return
 	}
-	return nil
+	if node.pre == nil {
+		c.head = c.head.next
+		c.head.pre = nil
+	} else {
+		node.pre.next = node.next
+	}
+	if node.next == nil {
+		c.tail = c.tail.pre
+		c.tail.next = nil
+	} else {
+		node.next.pre = node.pre
+	}
+	c.length--
+}
+
+// Get 获取kv
+func (c *LRUCache) Get(key int) int {
+	node, exist := c.store[key]
+	if !exist {
+		return -1
+	}
+	c.Delete(key)
+	c.Put(node.key, node.value)
+	return node.value
+}
+
+// Put 插入
+func (c *LRUCache) Put(key int, value int) {
+	c.Delete(key)
+	if c.length+1 > c.capacity {
+		c.Delete(c.tail.key)
+	}
+	node := LRUChainNode{key: key, value: value}
+	if c.length == 0 {
+		c.head = &node
+		c.tail = &node
+		c.store[key] = &node
+		c.length++
+		return
+	}
+	// 头部处理
+	c.head.pre = &node
+	node.next = c.head
+	c.head = &node
+	c.store[key] = &node
+	c.length++
 }
