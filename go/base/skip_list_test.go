dc46c0646a4574aa8d736084a38b8ed7a27c8814
qieguo <qieguo.chow@gmail.com>
2019-07-21 20:58:28 +0800

    feat-base: add skiplist
    Change-Id: I4e593eca9f77ebe364778d48a101ea84f07ed0f5

---
+++ go/base/skip_list_test.go
@@ -0,0 +1,41 @@
+package base
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestSkipList(t *testing.T) {
+	testcase := [][]int{}
+	testcaseStr := `[
+		[10,10],
+		[9,9],
+		[10,100],
+		[15,15],
+		[8,8],
+		[3,3],
+		[8,80],
+		[21,21],
+		[25,25],
+		[32,32]
+	]`
+
+	json.Unmarshal([]byte(testcaseStr), &testcase)
+
+	s := NewSkipList()
+	for i, data := range testcase {
+		s.Insert(data[0], data[1])
+		fmt.Printf("test[%d]: skiplist insert, index=%d, data=%+v\n", i, data[0], data[1])
+	}
+	fmt.Println("====== delete =======")
+	s.Delete(9)
+	s.Delete(21)
+	fmt.Println("====== search =======")
+	for i, data := range testcase {
+		index := data[0]
+		node := s.Search(index)
+		fmt.Printf("test[%d]: skiplist search, index=%d, data=%+v\n", i, index, node)
+	}
+	fmt.Printf("skiplist rank (2, 3) = %+v\n", s.Rank(2, 3))
+}
