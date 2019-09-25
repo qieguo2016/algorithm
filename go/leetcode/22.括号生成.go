cbbad59b16b619c29ddaacc482e6d737505a4839
qieguo <qieguo.chow@gmail.com>
2019-09-25 23:54:18 +0800

    组合: 括号组合
    Change-Id: I8ff4748a33a5bc413283ea1174534cd237a093d7

--- go/leetcode/22.括号生成.go
+++ go/leetcode/22.括号生成.go
@@ -6,9 +6,11 @@
  * https://leetcode-cn.com/problems/generate-parentheses/description/
  *
  * algorithms
- * Medium (68.25%)
- * Total Accepted:    15K
- * Total Submissions: 21.9K
+ * Medium (70.38%)
+ * Likes:    385
+ * Dislikes: 0
+ * Total Accepted:    24.1K
+ * Total Submissions: 34.2K
  * Testcase Example:  '3'
  *
  * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
@@ -25,7 +27,26 @@
  * 
  * 
  */
+
+func helper(left int, right int, out string, res *[]string) {
+	if left > right {
+		return
+	}
+	if left == 0 && right == 0 {
+		*res = append(*res, out) // 复制值
+		return 
+	}
+	if left > 0 {
+		helper(left-1, right, out+"(", res)
+	}
+	if right > 0 {
+		helper(left, right-1, out+")", res)
+	}
+}
+
 func generateParenthesis(n int) []string {
-    
+	res := []string{}
+	helper(n, n, "", &res)
+	return res
 }
 
