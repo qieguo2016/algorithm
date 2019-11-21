753b4eb77353868f9ccaffcf4c3b1f3f82d4a8fb
qieguo <qieguo.chow@gmail.com>
2019-11-21 10:22:25 +0800

    树: 二叉搜索树
    Change-Id: Iffc0850406ca16c8b06515e3b3b571baf2ec4b70

---
+++ go/leetcode/98.验证二叉搜索树.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (27.53%)
+ * Likes:    291
+ * Dislikes: 0
+ * Total Accepted:    40.8K
+ * Total Submissions: 147.4K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 假设一个二叉搜索树具有如下特征：
+ * 
+ * 
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	var INT_MAX = int(^uint(0) >> 1)
+	var INT_MIN = ^INT_MAX
+    return helper(root, INT_MIN, INT_MAX)
+}
+
+func helper(root *TreeNode, min int, max int) bool {
+	if root == nil {
+		return true
+	}
+	
+	if root.Val >= max || root.Val <= min  {
+		return false
+	}
+	return helper(root.Left, min, root.Val) && helper(root.Right, root.Val, max)
+}
+// @lc code=end
+
