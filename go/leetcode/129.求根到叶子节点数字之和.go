300c1b6765e3fb998855c063872b7beaef332ed0
qieguo <qieguo.chow@gmail.com>
2019-12-01 00:33:29 +0800

    树dfs: 叶子节点总和
    Change-Id: I1abbb2e597ebd337a7acd69f3d3e824fd31f8e35

---
+++ go/leetcode/129.求根到叶子节点数字之和.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=golang
+ *
+ * [129] 求根到叶子节点数字之和
+ *
+ * https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (59.92%)
+ * Likes:    92
+ * Dislikes: 0
+ * Total Accepted:    13.4K
+ * Total Submissions: 22.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ *
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ *
+ * 计算从根到叶子节点生成的所有数字之和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ *
+ * 示例 2:
+ *
+ * 输入: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ *
+ */
+
+// package leetcode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+	return dfs(root, 0)
+}
+
+func dfs(root *TreeNode, parent int) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val + parent
+	}
+	parent = 10 * (parent + root.Val) // 40 | 490
+	return dfs(root.Left, parent) + dfs(root.Right, parent)
+}
+
+// @lc code=end
