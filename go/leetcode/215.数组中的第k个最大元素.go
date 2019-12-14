451713fd4d068583387464c182de34c3b1f68c52
qieguo <qieguo.chow@gmail.com>
2019-12-14 14:10:27 +0800

    堆: top k
    Change-Id: If00c7e780ccbfdd21db98464e6910a39a8625fb0

---
+++ go/leetcode/215.数组中的第k个最大元素.go
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=golang
+ *
+ * [215] 数组中的第K个最大元素
+ *
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (59.34%)
+ * Likes:    293
+ * Dislikes: 0
+ * Total Accepted:    58.7K
+ * Total Submissions: 98.9K
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ *
+ * 说明:
+ *
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ *
+ */
+
+// top k，第一反应用堆，构造一个大小为k的最小堆，判断堆顶元素
+// package leetcode
+
+// @lc code=start
+import (
+	"container/heap"
+)
+
+type myHeap []int
+
+func (h *myHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
+}
+
+func (h *myHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *myHeap) Len() int {
+	return len(*h)
+}
+
+func (h *myHeap) Pop() (v interface{}) {
+	*h, v = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
+}
+
+func (h *myHeap) Push(v interface{}) {
+	*h = append(*h, v.(int))
+}
+
+func findKthLargest(nums []int, k int) int {
+	h := make(myHeap, 0)
+	heap.Init(&h)
+	for _, cur := range nums {
+		if h.Len() >= k {
+			if cur <= h[0] {
+				continue
+			}
+			heap.Pop(&h)
+		}
+		heap.Push(&h, cur)
+	}
+	return h[0]
+}
+
+// @lc code=end
