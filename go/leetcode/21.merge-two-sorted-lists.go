1437af302e3affd1e24d8601f9a4c803f2218e95
qieguo <qieguo.chow@gmail.com>
2020-06-08 12:51:31 +0800

    feat: update
    Change-Id: I9e30d8ccafcc319b652f95ffbe64e4de515da8a5

--- go/leetcode/21.merge-two-sorted-lists.go
+++ go/leetcode/21.merge-two-sorted-lists.go
@@ -28,8 +28,11 @@
  * }
  */
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil && l2 == nil {
-		return nil
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
 	}
 	cn := &ListNode{}
 	ret := &ListNode{Next: cn} // 预加一个链表头，方便遍历
