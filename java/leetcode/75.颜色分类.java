4c19230b466fe45bcdacce69ee52e9a84c025f52
qieguo <qieguo.chow@gmail.com>
2020-07-18 22:49:32 +0800

    双指针: 国旗问题
    Change-Id: I9299057ea95079241aa999824b148399cea90c5c

---
+++ java/leetcode/75.颜色分类.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=75 lang=java
+ *
+ * [75] 颜色分类
+ *
+ * https://leetcode-cn.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (52.75%)
+ * Likes:    266
+ * Dislikes: 0
+ * Total Accepted:    43K
+ * Total Submissions: 79.9K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ * 
+ * 注意:
+ * 不能使用代码库中的排序函数来解决这道题。
+ * 
+ * 示例:
+ * 
+ * 输入: [2,0,2,1,1,0]
+ * 输出: [0,0,1,1,2,2]
+ * 
+ * 进阶：
+ * 
+ * 
+ * 一个直观的解决方案是使用计数排序的两趟扫描算法。
+ * 首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+ * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums.length <= 1) {
+            return;
+        }
+        int left = 0, current = 0, right = nums.length - 1;
+        // 2,0,2,1,1,0
+        while (current <= right) {
+            if (nums[current] == 0) {
+                swap(nums, current, left);  // 0,2,2,1,1,0
+                left++;
+                current++;
+            } else if (nums[current] == 2) {
+                swap(nums, current, right);
+                right--;
+            } else {
+                current++;  // 1
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp;
+        tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+// @lc code=end
