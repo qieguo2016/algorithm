4bcdbd6f5a57820228c65ae2b79f51757aadcc1d
qieguo <qieguo.chow@gmail.com>
2020-07-21 20:42:17 +0800

    tree: 2叉树展开
    Change-Id: Id081d2d980ce042c977c4173fa64c645eff05936

---
+++ go/leetcode/114.二叉树展开为链表.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=golang
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (69.55%)
+ * Likes:    424
+ * Dislikes: 0
+ * Total Accepted:    51.6K
+ * Total Submissions: 74.2K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ * 
+ * 
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// 递归处理即可
+func flatten(root *TreeNode)  {
+	if root == nil {
+		return 
+	}
+	flatten(root.Left)
+	flatten(root.Right)
+	tmp := root.Right
+	root.Right = root.Left
+	root.Left = nil
+	for root.Right != nil {
+		root = root.Right
+	}
+	root.Right = tmp
+}
+// @lc code=end
+
