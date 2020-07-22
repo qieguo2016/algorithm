3093cbc5204f224d38e10cc2f6a76cef19d1e6fa
qieguo <qieguo.chow@gmail.com>
2020-07-22 09:34:14 +0800

    tree: 连接左右节点
    Change-Id: Ia04024017775ed97eb6ee2369d33a33359363834

---
+++ java/leetcode/117.填充每个节点的下一个右侧节点指针-ii.java
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=java
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ *
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (40.63%)
+ * Likes:    81
+ * Dislikes: 0
+ * Total Accepted:    10.1K
+ * Total Submissions: 23.9K
+ * Testcase Example:  '{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}'
+ *
+ * 给定一个二叉树
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * 
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * 
+ * 
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+ * 
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        if (root.left != null && root.right != null) {
+            root.left.next = root.right;
+        }
+        Node nextRoot = root.next;
+        while (nextRoot != null && nextRoot.left == null && nextRoot.right == null) {
+            nextRoot = nextRoot.next;
+        }
+        if (nextRoot != null) {
+            Node p = root.right, n = nextRoot.left;  // 前面已经判断root和nextRoot必有子节点
+            if (root.right == null) {
+                p = root.left;
+            }
+            if (nextRoot.left == null) {
+                n = nextRoot.right;
+            }
+            p.next = n;  
+        }
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+}
+// @lc code=end
+
