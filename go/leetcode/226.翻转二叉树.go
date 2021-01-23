171cc2b994d949c20d0a7d3b5f7e86909a7a2205
qieguo <qieguo.chow@gmail.com>
2021-01-23 23:49:59 +0800

    递归: 反转2叉树
    Change-Id: Ic0b66f5810d47086687f63575f8e5120953bdbef

---
+++ go/leetcode/226.翻转二叉树.go
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (77.86%)
+ * Likes:    733
+ * Dislikes: 0
+ * Total Accepted:    181.4K
+ * Total Submissions: 232.9K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 翻转一棵二叉树。
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ * 
+ * 输出：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ * 
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	l := invertTree(root.Right)
+	r := invertTree(root.Left)
+	root.Left = l
+	root.Right = r
+	return root
+}
+// @lc code=end
+
