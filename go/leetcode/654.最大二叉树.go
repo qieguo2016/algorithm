f933b362e05382e17280ef09f6d685dcf407091e
qieguo <qieguo.chow@gmail.com>
2021-01-23 23:41:08 +0800

    递归: 最大二叉树
    Change-Id: I081cc91f4dec1de0778c6c14b199ed996751ba09

---
+++ go/leetcode/654.最大二叉树.go
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=golang
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode-cn.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (81.41%)
+ * Likes:    235
+ * Dislikes: 0
+ * Total Accepted:    30.9K
+ * Total Submissions: 37.9K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
+ * 
+ * 
+ * 二叉树的根是数组 nums 中的最大元素。
+ * 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+ * 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
+ * 
+ * 
+ * 返回有给定数组 nums 构建的 最大二叉树 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * nums 中的所有整数 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	if len(nums) <= 0 {
+		return nil
+	}
+	idx := 0
+	max := nums[idx]
+	for i, v := range nums {
+		if max < v {
+			max = v
+			idx = i
+		}
+	}
+
+	return &TreeNode{
+		Val: max,
+		Left: constructMaximumBinaryTree(nums[0:idx]),
+		Right: constructMaximumBinaryTree(nums[idx+1: len(nums)]),
+	}
+}
+// @lc code=end
+
