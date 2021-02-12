dd42df27a589a9ebfbabad1f04806a4448ec151a
qieguo <qieguo.chow@gmail.com>
2021-02-12 16:21:29 +0800

    回溯: 子集
    Change-Id: Ia96cf99e0d4394949b0eabc0549a6aab8d15d47c

--- go/leetcode/78.子集.go
+++ go/leetcode/78.子集.go
@@ -5,16 +5,37 @@
  */
 
 // subsets 从空集开始，遍历时每次对结果集合内的每个结果继续叠加
+// func subsets(nums []int) [][]int {
+// 	ret := [][]int{}
+// 	ret = append(ret, []int{})
+// 	for i := 0; i < len(nums); i++ {
+// 		for _, sub := range ret {
+// 			sub = append(sub, nums[i])
+// 			tmp := append([]int{}, sub...)
+// 			ret = append(ret, tmp)
+// 		}
+// 	}
+// 	return ret
+// }
+
+// 用回溯法，组合模式来做
 func subsets(nums []int) [][]int {
-	ret := [][]int{}
-	ret = append(ret, []int{})
-	for i := 0; i < len(nums); i++ {
-		for _, sub := range ret {
-			sub = append(sub, nums[i])
-			tmp := append([]int{}, sub...)
-			ret = append(ret, tmp)
-		}
-	}
-	return ret
+	result := [][]int{}
+	path := []int{}
+	helper(nums, &result, &path, 0)
+	return result
 }
 
+func helper(nums []int, result *[][]int, path *[]int, start int) {
+	if start > len(nums) {
+		return
+	}
+	
+	*result = append(*result, append([]int{}, (*path)...))
+
+	for i := start; i < len(nums); i++ {
+		*path = append(*path, nums[i])
+		helper(nums, result, path, i+1)
+		*path = (*path)[:len(*path)-1]
+	}
+}
