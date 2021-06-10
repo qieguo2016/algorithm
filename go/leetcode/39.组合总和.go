87e8ba9082d5fd3b245d9d0fa358c0230904f86d
qieguo <qieguo.chow@gmail.com>
2021-06-11 00:49:53 +0800

    add
    Change-Id: Ibabd6aa8eb04597432cb4ae3a294ff9d3039ccad

--- go/leetcode/39.组合总和.go
+++ go/leetcode/39.组合总和.go
@@ -14,28 +14,28 @@
  * Testcase Example:  '[2,3,6,7]\n7'
  *
  * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
- * 
+ *
  * candidates 中的数字可以无限制重复被选取。
- * 
+ *
  * 说明：
- * 
- * 
+ *
+ *
  * 所有数字（包括 target）都是正整数。
- * 解集不能包含重复的组合。 
- * 
- * 
+ * 解集不能包含重复的组合。
+ *
+ *
  * 示例 1:
- * 
+ *
  * 输入: candidates = [2,3,6,7], target = 7,
  * 所求解集为:
  * [
  * ⁠ [7],
  * ⁠ [2,2,3]
  * ]
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: candidates = [2,3,5], target = 8,
  * 所求解集为:
  * [
@@ -43,30 +43,40 @@
  * [2,3,3],
  * [3,5]
  * ]
- * 
+ *
  */
 
-// 求所有解的组合一般可以考虑使用递归实现，先往解集合中添加某个元素，递归后如果超过限制则回退，恰好符合则将解加入结果集，若未到达限制则继续尝试添加。
+func combinationSum(candidates []int, target int) [][]int {
+	s := &solution{
+		arr: candidates,
+		out: []int{},
+		res: [][]int{},
+	}
+	s.helper(target, 0)
+	return s.res
+}
+
+type solution struct {
+	arr []int
+	out []int
+	res [][]int
+}
+
+// 求所有解的组合一般可以考虑使用递归实现
+// 先往解集合中添加某个元素，递归后如果超过限制则回退，恰好符合则将解加入结果集，若未到达限制则继续尝试添加。
 // 由于本题已经限定无重复元素，且元素可以重复选取，那么就是每次先递归同一个元素
-func helper(arr []int, target int, start int, out *[]int, res *[][]int) {
+func (s *solution) helper(target, start int) {
 	if target < 0 {
 		return
 	}
 	if target == 0 {
-		*res = append(*res, append([]int{}, (*out)...))  // 复制值
-		return 
+		s.res = append(s.res, append([]int{}, s.out...))
+		return
 	}
-	for i := start; i < len(arr); i++ {
-		*out = append(*out, arr[i])
-		helper(arr, target-arr[i], i, out, res)
-		*out = (*out)[:len(*out)-1]
+	for i := start; i < len(s.arr); i++ {
+		s.out = append(s.out, s.arr[i])
+		s.helper(target-s.arr[i], i)
+		s.out = s.out[:len(s.out)-1]
 	}
-}
-
-func combinationSum(candidates []int, target int) [][]int {
-	out := []int{}
-	res := [][]int{}
-	helper(candidates, target, 0, &out, &res)
-	return res
 }
 
--- go/leetcode/41.缺失的第一个正数.go
+++ go/leetcode/41.缺失的第一个正数.go
@@ -14,44 +14,45 @@
  * Testcase Example:  '[1,2,0]'
  *
  * 给定一个未排序的整数数组，找出其中没有出现的最小的正整数。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: [1,2,0]
  * 输出: 3
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: [3,4,-1,1]
  * 输出: 2
- * 
- * 
+ *
+ *
  * 示例 3:
- * 
+ *
  * 输入: [7,8,9,11,12]
  * 输出: 1
- * 
- * 
+ *
+ *
  * 说明:
- * 
+ *
  * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的空间。
- * 
+ *
  */
 
 // 原地交换排序，两次遍历
 // 交换后的数组要符合[1,2,3,4... ]的规则，第一个不符合这样规则的位置，就是结果
 func firstMissingPositive(nums []int) int {
-	for i := 0; i < len(nums); i++ {
-		for nums[i] > 0 && nums[i] < len(nums) && nums[nums[i] - 1] != nums[i] {
-      nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+	size := len(nums)
+	for i := 0; i < size; i++ {
+		for nums[i] > 0 && nums[i] < size && nums[i] != nums[nums[i]-1] { // 1应该在0位
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
 	}
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < size; i++ {
 		if nums[i] != i+1 {
-			return i+1
+			return i + 1
 		}
 	}
-	return len(nums)+1
+	return size + 1
 }
 
