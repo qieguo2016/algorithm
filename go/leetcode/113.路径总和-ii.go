583f30df46cfb1a89edaf224ef4246745c6625f5
qieguo <qieguo.chow@gmail.com>
2021-06-23 20:46:58 +0800

    整理代码
    Change-Id: I059e3385abb447c7477e1368cd9ff84bb5350f4c

--- go/leetcode/113.路径总和-ii.go
+++ go/leetcode/113.路径总和-ii.go
@@ -14,12 +14,12 @@
  * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
  *
  * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
- * 
+ *
  * 说明: 叶子节点是指没有子节点的节点。
- * 
+ *
  * 示例:
  * 给定如下二叉树，以及目标和 sum = 22，
- * 
+ *
  * ⁠             5
  * ⁠            / \
  * ⁠           4   8
@@ -27,16 +27,16 @@
  * ⁠         11  13  4
  * ⁠        /  \    / \
  * ⁠       7    2  5   1
- * 
- * 
+ *
+ *
  * 返回:
- * 
+ *
  * [
  * ⁠  [5,4,11,2],
  * ⁠  [5,8,4,5]
  * ]
- * 
- * 
+ *
+ *
  */
 /**
  * Definition for a binary tree node.
@@ -47,32 +47,33 @@
  * }
  */
 
-func helper(root *TreeNode, sum int)  {
+func pathSum(root *TreeNode, sum int) [][]int {
+	s := &solution{
+		ret:  [][]int{},
+		path: []int{},
+	}
+	s.helper(root, sum)
+	return s.ret
+}
+
+type solution struct {
+	ret  [][]int
+	path []int
+}
+
+func (s *solution) helper(root *TreeNode, sum int) {
 	if root == nil {
 		return
 	}
-	path = append(path, root.Val)
-	tmp := append([]int{}, path...)   // copy
 	rest := sum - root.Val
+	s.path = append(s.path, root.Val)
+	defer func() {
+		s.path = s.path[0 : len(s.path)-1]
+	}()
 	if root.Left == nil && root.Right == nil && rest == 0 {
-		ret = append(ret, tmp)   // use copy, not ref
+		s.ret = append(s.ret, append([]int{}, s.path...))
 		return
 	}
-	helper(root.Left, rest)
-	path = tmp    // remember reset path
-	helper(root.Right, rest)
-	if len(path) > 0 {
-		path = path[0:len(path)-1]
-	}
+	s.helper(root.Left, rest)
+	s.helper(root.Right, rest)
 }
-
-var ret [][]int
-var path []int
-
-func pathSum(root *TreeNode, sum int) [][]int {
-	ret = [][]int{}
-	path = []int{}
-	helper(root, sum)
-	return ret
-}
-
